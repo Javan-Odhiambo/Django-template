72a650e85ad2c265d8207ea9c5de75f4281045c9
Javan-Odhiambo <javan.o.otieno@gmail.com>
2023-08-13 16:28:20 +0300

    Initial commit

---
+++ config/settings/test.py
@@ -0,0 +1,15 @@
+# flake8: noqa: F403, F401
+from .base import *
+
+PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
+
+
+class DisableMigrations:
+    def __contains__(self, item):
+        return True
+
+    def __getitem__(self, item):
+        return None
+
+
+MIGRATION_MODULES = DisableMigrations()
