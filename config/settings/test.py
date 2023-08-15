b96803d344f3f440a53bbc41d0ace6aaa5e9ba34
Javan Odhiambo <114527546+Javan-Odhiambo@users.noreply.github.com>
2023-08-15 14:03:47 +0000

    Updated test settings to include database

--- config/settings/test.py
+++ config/settings/test.py
@@ -13,3 +13,29 @@ class DisableMigrations:
 
 
 MIGRATION_MODULES = DisableMigrations()
+
+# ==============================================================================
+# DATABASES SETTINGS
+# ==============================================================================
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+# ==============================================================================
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME", default="postgres_db", cast=str),
+        "USER": config("DB_USER", default="postgres_user", cast=str),
+        "PASSWORD": config("DB_PASS", default="postgres_pass", cast=str),
+        "HOST": config("DB_HOST", default="localhost", cast=str),
+        "PORT": config("DB_PORT", default="5432", cast=str),
+    }
+}
+
+if config("USE_SQLITE", default=False, cast=bool):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
